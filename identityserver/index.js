5353a8de28f94b67cbc3cb7b25c6626a88bb9cce
Motheo Moiloa <motheo@bbd.co.za>
2023-06-27 17:08:59 +0200

    Add https

--- identityserver/index.js
+++ identityserver/index.js
@@ -1,5 +1,7 @@
 import level from 'level';
 import dotenv from 'dotenv';
+import https from 'https';
+import fs from 'fs';
 import express from 'express';
 import { port } from './config/config.js';
 import handleRegisterRoute from './handlers/register.js';
@@ -33,10 +35,30 @@ app.use(bodyParser.json());
 app.locals.store = new level.Level('./store');
 
 //routes
+app.get('/', (req, res) => {
+    res.send("Entry Point");
+})
 app.post('/register', handleRegisterRoute);
 app.post('/login', handleExchangeCredentialsRoute);
 app.get('/verify-authentication', bearer, (request, response) => response.send('Your are authenticated'));
 
-app.listen(port, () => {
+// Read SSL certificate and private key files
+const privateKey = fs.readFileSync('privateKey.pem', 'utf8');
+const certificate = fs.readFileSync('certificate.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+  });
+  
+  app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  });
+
+httpsServer.listen(port, () => {
     console.log(`Identity server is running on port ${port} ...`);
 });
